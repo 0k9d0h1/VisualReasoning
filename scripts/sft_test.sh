93a98575f9e39ee56d47e82eb5903f917dd04334
Donghun Kang <0k9d0h1@snu.ac.kr>
2025-05-06 23:14:11 +0900

    init

---
+++ scripts/sft_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=4 ray start --head
+export HF_HOME="/home/kdh0901/Desktop/cache_dir/kdh0901"
+set -x
+
+if [ "$#" -lt 2 ]; then
+    echo "Usage: run_qwen_05_sp2.sh <nproc_per_node> <save_path> [other_configs...]"
+    exit 1
+fi
+
+nproc_per_node=$1
+save_path=$2
+
+# Shift the arguments so $@ refers to the rest
+shift 2
+
+CUDA_VISIBLE_DEVICES=4 torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
+     -m verl.trainer.fsdp_sft_trainer \
+    data.train_files=./data/visual7w_train.parquet \
+    data.val_files=./data/visual7w_val.parquet \
+    data.multiturn.enable=true \
+    data.multiturn.messages_key=messages \
+    data.micro_batch_size=4 \
+    model.partial_pretrain=Qwen/Qwen2.5-VL-3B-Instruct \
+    trainer.default_local_dir=$save_path \
+    trainer.project_name=test \
+    trainer.experiment_name=test-sft-qwen-2.5-vl-3b-instruct-sp2 \
+    trainer.logger=['console','wandb'] \
+    trainer.total_training_steps=1 \
+    trainer.default_hdfs_dir=null $@ \
+    ulysses_sequence_parallel_size=1 
